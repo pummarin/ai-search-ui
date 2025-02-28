7c6f2715f44898a7cf04d4d410454dd4fee3d9c6
pummarin <pummarinb6003296@gmail.com>
2025-02-28 08:11:10 +0700

    change routing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Chat from './view/Chat';
+import Chat from './view/chat/Chat';
 import Layout from './view/layout/Layout';
 
 function App() {
